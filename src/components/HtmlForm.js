7708841ae635aa72e5a2bbcd659581fad61b2730
Ashesh <83585384+ashesh790@users.noreply.github.com>
2022-12-25 22:45:16 +0530

    Light and Dark mode enabled

--- src/components/HtmlForm.js
+++ src/components/HtmlForm.js
@@ -9,14 +9,40 @@ export default function HtmlForm(properties) {
         let CapText = text.toUpperCase();
         setText(CapText);
     }
-    const [text, setText] = useState('Initial Value')
+
+    const [text, setText] = useState('')
+    const [myStyle, setMyStyle] = useState(
+        {
+            color: "black",
+            backgroundColor: "white"
+        }
+    )
+    const changeMode = () => {
+        if (myStyle.color === "black") {
+            setMyStyle({
+                color: "white",
+                backgroundColor: "black"
+            });
+        }
+        else {
+            setMyStyle({
+                color: "black",
+                backgroundColor: "white"
+            })
+        }
+    }
     return (
         <>
-            <h1>{properties.heading}</h1>
-            <div>
-                <textarea rows={10} id="my-box" value={text} onChange={textChange} className='my-box-class form-control'></textarea>
-                <button className="btn btn-primary my-2" onClick={textUpper}>Check</button>
-                <p className='my-2'>{text.length}-{text.split(" ").length - 1}</p>
+            <div style={myStyle}>
+                <h1>{properties.heading}</h1>
+                <div>
+                    <textarea rows={10} id="my-box" value={text} style={myStyle} onChange={textChange} className='my-box-class form-control'></textarea>
+                    <button className="btn btn-primary my-2" onClick={textUpper}>Check</button>
+                    <p className='my-2'>{text.length} Characters and {text.split(" ").length - 1} Words</p>
+                    <h2>Preview</h2>
+                    <p>{text}</p>
+                    <button className="btn btn-primary" onClick={changeMode}>Change Color</button>
+                </div>
             </div>
         </>
     )
